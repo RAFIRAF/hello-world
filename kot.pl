bd39f78d96b7f82557eaf3cb6e997f1df9d36e88
RAFIRAF <rafalplaszczyk.com@gmail.com>
2016-03-31 18:12:12 +0200

    Update kot.pl

--- kot.pl
+++ kot.pl
@@ -9,3 +9,7 @@ we(X,Y,Z):-ma(X,Z),ma(Y,Z).
 %we(X,Y,Z):-ma(X,Z),write([X,Z]),ma(Y,Z),write([Y,Z]),X@<Y.
 %findall((X,Y,Z),we(X,Y,Z),L). -- KOMENDA (drzewa, liście????)
 %member(3,[1,2,3,4,5]). -- KOOMENDA. FALSE BO PRÓBUJE SZUKAĆ DALEJ.
+
+run(X) :- X.
+%run(ma(X,Y)).
+%run(we(X,Y,Z)).
