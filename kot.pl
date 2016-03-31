bf611a876dfdcee6aaebd5106cb175825df3f152
RAFIRAF <rafalplaszczyk.com@gmail.com>
2016-03-31 17:00:36 +0200

    Create kot.pl

---
+++ kot.pl
@@ -0,0 +1,11 @@
+ma(ala,kot).
+ma(ala,pies).
+ma(jacek,kot).
+
+%we(X,Y):-ma(X,Z),ma(Y,Z). 
+we(X,Y,Z):-ma(X,Z),ma(Y,Z).
+%we(X,Y,Z):-ma(X,Z),ma(Y,Z),X\=Y.
+%we(X,Y,Z):-ma(X,Z),ma(Y,Z),X@>Y. % j>a
+%we(X,Y,Z):-ma(X,Z),write([X,Z]),ma(Y,Z),write([Y,Z]),X@<Y.
+%findall((X,Y,Z),we(X,Y,Z),L). -- KOMENDA (drzewa, liście????)
+%member(3,[1,2,3,4,5]). -- KOOMENDA. FALSE BO PRÓBUJE SZUKAĆ DALEJ.
